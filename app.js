c7db634367e4f0917ecfb6a22e26229110524bb9
Igor Ivanov <drkierkegor@gmail.com>
2020-10-12 23:43:53 +0300

    redesign :: 1ly for root list

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
 "use strict";
 
-const uiMessages = {
-    init: "Please, specify parent categories of the tree",
-};
-
 const domReady = () => {
     const tree = {};
 
@@ -13,50 +9,42 @@ const domReady = () => {
 
     // subcategory
     const addSubcategoryBtn = document.querySelector(".btn-add-subcat");
-    const subcategoryValueClear = document.querySelector(".btn-subcat-clear");
     const subcategoryInput = document.getElementById("new-subcategory");
     const subcatForm = document.querySelector(".subcategory-form");
 
     const workingArea = document.querySelector(".working-area");
     const btnValueClear = document.querySelectorAll(".btn-clear");
     let workingList = document.querySelector(".root-list"); // default
-    let workingListId = 0; // default
     let currentId = 0;
     let newCategoryValue, newSubcategoryValue = "";
 
-    addCategoryBtn.addEventListener( "click", function() {
-        const items = document.querySelectorAll(".list-item");
+    // adds a category
+    addCategoryBtn.addEventListener( "click", () => {
         newCategoryValue = categoryInput.value;
 
         workingList.insertAdjacentHTML( "beforeend", 
             `<li id="${currentId}" class="list-item">${newCategoryValue}
-                <span class="control add-subcategory">+</span>
-                <span class="control remove-subcategory">-</span>
+                <span class="control add-item">+</span>
+                <span class="control remove-item">-</span>
+                <span class="control text-control edit-item">edit</span>
             </li>`
         );
         // clear immediately after inserting
         categoryInput.value = "";
 
-        const itemsLength = items.length;
-        // test
-        console.log(`Items length: ${itemsLength + 1}`);
-
         tree[currentId] = {};
         tree[currentId].val = newCategoryValue;
 
-        // test
-        console.log(tree);
-        console.log(`Current value: ${tree[currentId].val}`);
+        console.log(`Current value: ${tree[currentId].val}`); // test
         currentId++;
-        // test
-        console.log(`An item ID after increment: ${currentId}`);
-    } );
+    });
 
-    // clear input's value (cat / subcat)
-    const btnValueClearLength = btnValueClear.length;
     
-    for (let i = 0; i < btnValueClearLength; i++) {
-        btnValueClear[i].addEventListener( "click", function(e) {
+    // clear input's value (cat / subcat)
+    const btnValueClearAmount = btnValueClear.length;
+    
+    for (let i = 0; i < btnValueClearAmount; i++) {
+        btnValueClear[i].addEventListener( "click", (e) => {
             const child = e.target;
             const parent = child.parentNode;
             const requiredInput = parent.querySelector(".form-field");
@@ -64,65 +52,42 @@ const domReady = () => {
         } );
     }
 
-    // handle adding of subcats
-    let child, parent = "";
 
-    workingArea.addEventListener( "click", function(e) {
+    // CRUD for categories / subcategories
+    let child, parent, currentLi = "";
+
+    workingArea.addEventListener( "click", (e) => {
         child = e.target;
 
         if ( !child.matches(".control") ) return;
 
-        // test
-        //child.style.color = "red";
-        
-        parent = child.parentNode;
+        parent = child.closest(".child-list") || child.closest(".root-list");
+        currentLi = child.closest(".list-item");
+        //currentLi.style.color = "red"; // test
 
-        // highlight active parent node
-        parent.classList.toggle("parent-active");
-        subcatForm.style.display = "block";
-    } );
+        if ( child.matches(".add-item") ) {
+            //
+        } else if ( child.matches(".edit-item") ) {
+            //
+        } else if ( child.matches(".remove-item") ) {
+            parent.removeChild(currentLi);
 
-    addSubcategoryBtn.addEventListener( "click", function() {
-        newSubcategoryValue = subcategoryInput.value;
-
-        /*
-        ## To-Do
-        - Add the value into the object 'tree'
-        */
-
-        // test
-        //console.log("Parent's children amount: " + parent.children.length);
-
-        const nestedList = parent.querySelector(".child-list");
-        
-        if (nestedList == null) {
-            console.log(`Parent has not such descendant.`);
-            // add child `ol` and insert trailing `li` with a value
-            parent.insertAdjacentHTML( "beforeend",
-                `<ol id="0" class="child-list list">
-                    <li id="0" class="list-item">${newSubcategoryValue}
-                        <span class="control add-subcategory">+</span>
-                        <span class="control remove-subcategory">-</span>
-                    </li>
-                </ol>`
-            );
-        } else {
-            // insert trailing `li` with a value
-            const lastChildId = nestedList.lastElementChild.id;
-            const lastChildIdInt = parseInt(lastChildId, 10);
-
-            nestedList.insertAdjacentHTML( "beforeend",
-                `<li id="${lastChildIdInt + 1}" class="list-item">${newSubcategoryValue}
-                    <span class="control add-subcategory">+</span>
-                    <span class="control remove-subcategory">-</span>
-                </li>`
-            );
+            // decrease ID
+            currentId--;
+            // test
+            console.log(`Current ID: ${currentId}.`);
+            
+            // sets proper IDs if they're not match
+            const items = parent.querySelectorAll(".list-item");
+            for (let i = 0; i < items.length; i++) {
+                const currentId = items[i].id;
+                const currentIdInt = parseInt(currentId, 10);
+                if (currentIdInt !== i) {
+                    items[i].id = i;
+                }
+            }
         }
-
-        parent.classList.toggle("parent-active");
-        subcategoryInput.value = "";
-        subcatForm.style.display = "none";
-    } );
+    });
 };
 
 document.addEventListener("DOMContentLoaded", domReady);
