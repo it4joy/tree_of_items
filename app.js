10d8cf612b8e3e9f7262e3f517ef61a49ca706c8
Igor Ivanov <drkierkegor@gmail.com>
2020-10-18 21:24:58 +0300

    upd edit & del for cat

--- app.js
+++ app.js
@@ -4,26 +4,30 @@ const domReady = () => {
     const tree = {};
 
     // category
+    const catForm = document.querySelector(".category-form");
     const addCategoryBtn = document.querySelector(".btn-add-cat");
+    const saveCategoryBtn = catForm.querySelector(".btn-save-cat");
     const categoryInput = document.getElementById("new-category");
 
     // subcategory
+    const subcatForm = document.querySelector(".subcategory-form");
     const addSubcategoryBtn = document.querySelector(".btn-add-subcat");
     const subcategoryInput = document.getElementById("new-subcategory");
-    const subcatForm = document.querySelector(".subcategory-form");
 
     const workingArea = document.querySelector(".working-area");
     const btnValueClear = document.querySelectorAll(".btn-clear");
     let workingList = document.querySelector(".root-list"); // default
     let currentId = 0;
     let newCategoryValue, newSubcategoryValue = "";
+    let child, parent, currentLi = "";
 
     // adds a category
     addCategoryBtn.addEventListener( "click", () => {
         newCategoryValue = categoryInput.value;
 
         workingList.insertAdjacentHTML( "beforeend", 
-            `<li id="${currentId}" class="list-item">${newCategoryValue}
+            `<li id="${currentId}" class="list-item">
+                <span class="item-value">${newCategoryValue}</span>
                 <span class="control add-item">+</span>
                 <span class="control remove-item">-</span>
                 <span class="control text-control edit-item">edit</span>
@@ -35,9 +39,30 @@ const domReady = () => {
         tree[currentId] = {};
         tree[currentId].val = newCategoryValue;
 
-        console.log(`Current value: ${tree[currentId].val}`); // test
+        // test +
+        console.log(`Current value: ${tree[currentId].val}`);
+        
         currentId++;
     });
+
+    // edit a category
+    saveCategoryBtn.addEventListener( "click", (e) => {
+        const updatedCategoryValue = categoryInput.value;
+        const btn = e.target;
+        // test +
+        //console.log(`Updated cat val: ${updatedCategoryValue}`);
+        currentLi.querySelector(".item-value").textContent = updatedCategoryValue;
+        // change property's value in the object ...
+        tree[currentId].val = updatedCategoryValue;
+        // test +
+        console.log(`Updated val in the obj: ${tree[currentId].val}`);
+
+        // return to initial state
+        btn.style.display = "none";
+        categoryInput.value = "";
+        catForm.querySelector(".state").textContent = "";
+        currentLi.classList.toggle("li-active");
+    } );
 
     
     // clear input's value (cat / subcat)
@@ -54,8 +79,6 @@ const domReady = () => {
 
 
     // CRUD for categories / subcategories
-    let child, parent, currentLi = "";
-
     workingArea.addEventListener( "click", (e) => {
         child = e.target;
 
@@ -68,14 +91,28 @@ const domReady = () => {
         if ( child.matches(".add-item") ) {
             //
         } else if ( child.matches(".edit-item") ) {
-            //
+            const itemValue = currentLi.querySelector(".item-value").textContent;
+            // test +
+            //console.log(`The value for edit: ${itemValue}`);
+            currentLi.classList.toggle("li-active");
+            catForm.querySelector(".state").textContent = " | Edit ...";
+            categoryInput.value = itemValue;
+            saveCategoryBtn.style.display = "inline-block";
         } else if ( child.matches(".remove-item") ) {
             parent.removeChild(currentLi);
+
+            // change object (N: think about parents / children)
+            // + upd properties (set corresponding, equal to IDs)
+            const id = currentLi.id;
+            const idInt = parseInt(id, 10);
+            delete tree[idInt];
+            // test
+            console.log(tree);
 
             // decrease ID
             currentId--;
             // test
-            console.log(`Current ID: ${currentId}.`);
+            console.log(`Current ID after removing an item: ${currentId}.`);
             
             // sets proper IDs if they're not match
             const items = parent.querySelectorAll(".list-item");
